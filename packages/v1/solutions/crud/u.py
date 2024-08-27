d856a975b9d959f8933c476f5f29db4499f76425
Jdiv <thinquingquietly@gmail.com>
2024-08-27 09:09:27 -0400

    update_solution is testable

--- packages/v1/solutions/crud/u.py
+++ packages/v1/solutions/crud/u.py
@@ -1,16 +1,33 @@
-from context.context import session
-def update_solution(user, user_type, assistant_type, agent_id, update_data):
+import cloudinary
+import cloudinary.uploader
+import os
+from context.context import session, update_table
+def update_solution(event, user, user_type):
     if user_type in ['admin', 'system_admin']:  # Assuming only admins can update
         try:
-            table_name = "{0}_assistants_{1}s_threads".format(assistant_type, user_type)
-            update_query = (f"UPDATE {table_name} SET ... WHERE id = :id RETURNING *")
-            results = session.execute(update_query, {"id": agent_id, **update_data})
-            session.commit()
-            updated_agent = results.fetchone()
-            return {"body": {"message": "AI Agent updated successfully", "agent": dict(updated_agent)}, "statusCode": 200}
+            headers = event.get('http', {}).get('headers', {})
+            content_type = headers.get('content-type', None)
+            if content_type.startswith('multipart/form-data'):
+                image = event.get('image', None)
+                print(image)
+                if image is not None:
+                    cloudinary.config( 
+                        cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME'), 
+                        api_key = os.environ.get('CLOUDINARY_API_KEY'), 
+                        api_secret = os.environ.get('CLOUDINARY_API_SECRET')
+                    )
+                    # Should get user and stripe_passport here (obviously taking shortcuts)
+                    # Don't forget to add the passport_id back into the folder name for cloudinary upload.
+                    cdn_response = cloudinary.uploader.upload(image, folder=f'customer_teams/solutions')
+                    update_dict = {'cover_image_url': cdn_response['secure_url']}
+                updated_solution = update_table("solution", 'public_id', "ff6cb38a-8e34-4efa-b829-5f82039e51bd", update_dict)
+
+                return {"body": {"message": "Solution updated successfully", "agent": ""}, "statusCode": 200}
+            else:
+                return {"body": "Content-Type incorrect or None", "statusCode": 400}
         except Exception as e:
             session.rollback()
-            print("Failed to update AI Agent:", str(e))
+            print("Failed to update Solution:", str(e))
             return {"body": "Internal Server Error", "statusCode": 500}
     else:
-        return {"body": "Unauthorized to update AI Agents", "statusCode": 403}
+        return {"body": "Unauthorized to update Solutions", "statusCode": 403}
