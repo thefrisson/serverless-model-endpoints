e22848d75df4f457aa8250e3452bab83ef19dfe3
Jdiv <thinquingquietly@gmail.com>
2024-09-10 17:43:40 -0400

    extended error catching on list_objects

--- packages/v1/objects/crud/r.py
+++ packages/v1/objects/crud/r.py
@@ -6,6 +6,8 @@ from resources import valid_objects, objects_a, objects_b, context_map
 def list_objects(user, user_type, obj, object_user_type, filters=None):
     if (user_type == "system_admin" and object_user_type in ['customer', 'admin', 'affiliate', 'system', 'end_user']) or (user_type == "admin" and object_user_type in ['customer', 'admin', 'affiliate', 'end_user']) or (user_type == "customer" and object_user_type in ['customer', 'affiliate', 'end_user']) or (user_type == "affiliate" and object_user_type in ['affiliate', 'end_user']) or (user_type == "end_user" and object_user_type in ['end_user']):
         try:
+            
+            table_name = None
             if obj in valid_objects:
                 
                 pre_table_name = obj[:-1] if obj.endswith('s') else obj
@@ -14,14 +16,18 @@ def list_objects(user, user_type, obj, object_user_type, filters=None):
                     table_name = f"{object_user_type}_{obj}"
                 elif pre_table_name in objects_b:
                     table_name = context_map[obj][object_user_type]
-            if filters is None:
-                objects = row_to_dict(select_from_table(table_name))
+            if table_name is not None:
+                if filters is None:
+                    objects = row_to_dict(select_from_table(table_name))
+                else:
+                    objects = row_to_dict(select_from_table(table_name, filters=filters))
+                
+                formatted_result = {'type': obj, 'data': objects, "statusCode": 200}
+                # Ensure fetching results first
+                return formatted_result
             else:
-                objects = row_to_dict(select_from_table(table_name, filters=filters))
+                return {'type': "error", 'error': "Not a Valid Object or similar error.", 'statusCode': 400}
 
-            formatted_result = {'type': obj, 'data': objects, "statusCode": 200}
-            # Ensure fetching results first
-            return formatted_result
         except Exception as e:
             print("Retrieving failed:", str(e))
             return {"body": "Internal Server Error", "statusCode": 500}
