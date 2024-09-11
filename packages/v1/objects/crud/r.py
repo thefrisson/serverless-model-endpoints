cad0bec05a5fd56304e7b49a9d2ff65e4bc60361
Jdiv <thinquingquietly@gmail.com>
2024-09-10 22:15:24 -0400

    added responses

--- packages/v1/objects/crud/r.py
+++ packages/v1/objects/crud/r.py
@@ -6,7 +6,6 @@ from resources.resources import valid_objects, objects_a, objects_b, context_map
 def list_objects(user, user_type, obj, object_user_type, filters=None):
     if (user_type == "system_admin" and object_user_type in ['customer', 'admin', 'affiliate', 'system', 'end_user']) or (user_type == "admin" and object_user_type in ['customer', 'admin', 'affiliate', 'end_user']) or (user_type == "customer" and object_user_type in ['customer', 'affiliate', 'end_user']) or (user_type == "affiliate" and object_user_type in ['affiliate', 'end_user']) or (user_type == "end_user" and object_user_type in ['end_user']):
         try:
-            
             table_name = None
             if obj in valid_objects:
                 
@@ -24,25 +23,29 @@ def list_objects(user, user_type, obj, object_user_type, filters=None):
                     return formatted_result
 
                 
-            elif pre_table_name in objects_b:
-                print("objects_b")
-                if filters is None:
-                    filters = {}
-                scopes_list = get_scoping_object(user, user_type, context_map[obj][object_user_type]['scope_objects'][user_type]['tablename'])
-                objects = []
-                for scope_object in scopes_list:
-                    if context_map[obj][object_user_type]['scope_objects'][user_type]['output'] in scope_object.keys():
-                        table_name = context_map[obj][object_user_type]['tablename']
+                elif pre_table_name in objects_b:
+                    print("objects_b")
+                    if filters is None:
+                        filters = {}
+                    scopes_list = get_scoping_object(user, user_type, context_map[obj][object_user_type]['scope_objects'][user_type]['tablename'])
+                    objects = []
+                    for scope_object in scopes_list:
+                        if context_map[obj][object_user_type]['scope_objects'][user_type]['output'] in scope_object.keys():
+                            table_name = context_map[obj][object_user_type]['tablename']
 
-                        object_iter = row_to_dict(select_from_table(table_name, filters=filters))
-                        objects.append(object_iter)
-                    
-                    if table_name is not None:
-                        formatted_result = {'type': obj, 'data': objects, "statusCode": 200}
-                        # Ensure fetching results first
-                        return formatted_result
-                    else:
-                        return {'type': "error", 'error': "Not a Valid Object or similar error for tembo object.", 'statusCode': 400}
+                            object_iter = row_to_dict(select_from_table(table_name, filters=filters))
+                            objects.append(object_iter)
+                        
+                        if table_name is not None:
+                            formatted_result = {'type': obj, 'data': objects, "statusCode": 200}
+                            # Ensure fetching results first
+                            return formatted_result
+                        else:
+                            return {'type': "error", 'error': "Not a Valid Object or similar error for tembo object.", 'statusCode': 400}
+                else:
+                    return {'type': "error", 'error': "Not a 3.", 'statusCode': 400}
+            else:
+                return {'type': "error", 'error': "Not a.", 'statusCode': 400}
 
         except Exception as e:
             print("Retrieving failed:", str(e))
