e3761b43780558997d7deb88cf561aba553441ad
Jdiv <thinquingquietly@gmail.com>
2024-09-10 21:47:49 -0400

    fixed valid_objects

--- packages/v1/objects/resources/resources.py
+++ packages/v1/objects/resources/resources.py
@@ -1,7 +1,7 @@
 valid_objects = [
     'explore_groups', 'external_account_type_generic_groups', 'solution_template_generic_groups', 'external_account_types', 'solution_templates',
     'solutions', 'assistant_templates', 'assistants', 'threads', 'messages', 'runs', 'priorities', 'carts', 'orders', 'invoices', 'files', 'domains', 'messages', 'threads', 'demos', 'events', 'opportunities',
-    'stripe_passports', 'users', 'meters', 'voting_topics', 'workflow_templates', 'invites'
+    'users', 'meters', 'voting_topics', 'workflow_templates', 'invites', 'passports'
 ]
 
 objects_a = ['external_account_type_generic_groups_explore_groups', 'solution_template_generic_groups_explore_groups', 'users_stripe_passports']
