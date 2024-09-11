b1cdc3a060a10f7e19ffa47c5dc48d569a2dd258
Jdiv <thinquingquietly@gmail.com>
2024-09-11 00:01:04 -0400

    try again

--- packages/v1/objects/scopes/scope.py
+++ packages/v1/objects/scopes/scope.py
@@ -5,7 +5,7 @@ from context.context import safe_getattr
 
 def get_scoping_object(user, user_type, scoping_table_name):
     resp = requests.get(
-        url=f"https://{os.environ.get('TEMBO_DATA_DOMAIN')}/restapi/v1/{scoping_table_name}",
+        url=f"https://{os.environ.get('TEMBO_DATA_DOMAIN')}/restapi/v1/dev.{scoping_table_name}",
         headers={"Authorization": f"Bearer {os.environ.get('TEMBO_TOKEN')}"},
         params={
             f'{user_type}_id': f"eq.{safe_getattr(user, f"{user_type}_id")}"
